a0b0e1c891b6ecb54f0a4c3c25b680121979c773
mariangggexo <mariana.seo09@gmail.com>
2020-10-13 18:58:37 -0500

    subiendo problema 1

---
+++ sitio1/solucion1.js
@@ -0,0 +1,14 @@
+var divhtml=document.getElementById("div_msg");
+var radio=document.getElementById("texto_n1");
+
+
+var boton=document.getElementById("boton_calcula");
+boton.addEventListener("click",radio_click);
+
+function radio_click()
+ {
+ 	pi=3.14159;
+ 	r=parseInt(radio1.value);
+	var radio =(4.0/3)*pi*r*r*r;
+	div_msg.innerHTML="El radio es:" + radio;
+}
