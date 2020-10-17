fdbd730875d0758ca645a891501c841954c94e47
mariangggexo <mariana.seo09@gmail.com>
2020-10-16 19:55:19 -0500

    solucion del problema 2 completada

--- sitio2/solucion2.js
+++ sitio2/solucion2.js
@@ -1,11 +1,42 @@
 var divhtml=document.getElementById("div_msg");
-var X=document.getElementById("cordenada1");
-var Y=document.getElementById("cordenada2");
+var valorX=document.getElementById("cordenada1");
+var valorY=document.getElementById("cordenada2");
 var boton=document.getElementById("boton_mostrar");
 boton.addEventListener("click",cordenada_click);
 
-function cordenada_click
+function cordenada_click()
 {
-	X=parseInt(cordenada1.value);
-	Y=parseInt(cordenada2.value);
+	
+	div_msg.innerHTML="Cuandrante:";
+	var x=parseInt(cordenada1.value);
+	var y=parseInt(cordenada2.value);
+
+	if (x > 0.0 && y > 0.0)
+	{	
+	texto.innerHTML = "Q1";
+	}
+	else if (x < 0.0 && y > 0.0)
+	{
+	texto.innerHTML = "Q2";
+	}
+	else if (x < 0.0 && y < 0.0)
+	{
+	texto.innerHTML = "Q3";
+	}
+	else if (x > 0.0 && y < 0.0)
+	{
+	texto.innerHTML = "Q4";
+	}
+	else if (x == 0.0 && y == 0.0)
+	{
+	texto.innerHTML = "Origem";
+	}
+	else if (x == 0.0 && y != 0.0)
+	{
+	texto.innerHTML = "Eixo Y";
+	}
+	else if (x != 0.0 && y == 0.0)
+	{
+	texto.innerHTML = "Eixo X";
+	}
 }
